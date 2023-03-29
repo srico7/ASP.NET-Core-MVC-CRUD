2da5a763243e0c9e63f4c8b611a7579221d36b76
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2023-03-30 01:21:53 +0530

    Form Added
    Form and database connection done

---
+++ NetMVCCRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetMVCCRUD.Data;
+using NetMVCCRUD.Models;
+using NetMVCCRUD.Models.Domain;
+
+namespace NetMVCCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCDbContext mVCDbContext;
+
+        public EmployeesController(MVCDbContext mVCDbContext)
+        {
+            this.mVCDbContext = mVCDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+            };
+
+            await mVCDbContext.Employees.AddAsync(employee);
+            await mVCDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ NetMVCCRUD/Data/MVCDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetMVCCRUD.Models.Domain;
+
+namespace NetMVCCRUD.Data
+{
+    public class MVCDbContext : DbContext
+    {
+        public MVCDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
